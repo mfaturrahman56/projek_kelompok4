1ef5485672fb2a3b1d14d454ae0ce2171ca933ba
alfanayrs12 <alfarabypro@gmail.com>
2023-07-05 10:56:54 +0700

    Update home.dart
    Penambahan Home.dart

--- lib/models/home.dart
+++ lib/models/home.dart
@@ -30,3 +30,145 @@ class HomePage extends StatelessWidget {
     );
   }
 }
+Widget buildSingleItem({
+    images,
+    title,
+    subTitle,
+    price,
+    context,
+    rating,
+  }) {
+    return GestureDetector(
+      onTap: () {
+      },
+      child: Container(
+        width: screenWidth * 0.4 + 10,
+        height: screenHeight * 0.3,
+        margin: EdgeInsets.all(15.0),
+        padding: EdgeInsets.all(10.0),
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+          color: Color(0xff17191f),
+          borderRadius: BorderRadius.circular(20.0),
+        ),
+        child: Stack(
+          alignment: Alignment.topRight,
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Expanded(
+                  flex: 2,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      boxShadow: [
+                        BoxShadow(
+                          blurRadius: 2.0,
+                          spreadRadius: 1.0,
+                          color: Color(0xff30221f),
+                        ),
+                      ],
+                      image: DecorationImage(
+                        fit: BoxFit.cover,
+                        image: AssetImage(images),
+                      ),
+                      borderRadius: BorderRadius.circular(20.0),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 10.0,
+                ),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Text(
+                        title,
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 3,
+                      ),
+                      Text(
+                        subTitle,
+                        style: TextStyle(
+                          color: Color(0xffaeaeae),
+                          fontSize: 10,
+                        ),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Row(
+                            children: [
+                              Text(
+                                "\$\t",
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    color: Color(0xffd17842)),
+                              ),
+                              Text(
+                                "$price",
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ],
+                          ),
+                          Container(
+                            decoration: BoxDecoration(
+                              color: Color(0xffd17842),
+                              borderRadius: BorderRadius.circular(7),
+                            ),
+                            child: Icon(
+                              Icons.add,
+                              size: 25,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            Container(
+              height: 20.0,
+              width: 55.0,
+              decoration: BoxDecoration(
+                color: Color(0xff231715),
+                borderRadius: BorderRadius.only(
+                  topRight: Radius.circular(20.0),
+                  bottomLeft: Radius.circular(15.0),
+                ),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Icon(
+                    Icons.star,
+                    size: 12,
+                    color: Color(0xffd17842),
+                  ),
+                  Text(
+                    "$rating",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
